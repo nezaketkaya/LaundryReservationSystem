253482b813e51b3343486875b2e3f4a8e71a90c8
nezaketkaya <kayanezak@hotmail.com>
2024-02-19 03:31:14 +0300

    Edit create

--- Controllers/BookingController.cs
+++ Controllers/BookingController.cs
@@ -2,6 +2,8 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
 
 namespace LaundryReservationSystem.Controllers
 {
@@ -31,70 +33,60 @@ namespace LaundryReservationSystem.Controllers
             }
         }
 
-          private IEnumerable<Machine> GetAvailableMachines(DateTime startTime, DateTime finishTime, string machineType)
-          {
-              var allMachines = _dbContext.Machines.ToList();
+        [HttpPost]
+        public IActionResult GetAvailableMachines(DateTime date, DateTime startTime, string machineType)
+        {
+            var endTime = startTime.Add(TimeSpan.FromHours(1));
 
-              var reservedMachines = _dbContext.Bookings
-                  .Where(b =>
-                      (startTime >= b.BookingTimeStart && startTime < b.BookingTimeFinish) ||
-                      (finishTime > b.BookingTimeStart && finishTime <= b.BookingTimeFinish) ||
-                      (startTime <= b.BookingTimeStart && finishTime >= b.BookingTimeFinish))
-                  .Select(b => b.Id)
-                  .Distinct()
-                  .ToList();
+            var reservedMachines = _dbContext.Bookings
+                .Where(b => b.BookingDate == date &&
+                            ((b.BookingTimeStart >= startTime && b.BookingTimeStart < endTime) ||
+                            (b.BookingTimeFinish > startTime && b.BookingTimeFinish <= endTime) ||
+                            (b.BookingTimeStart <= startTime && b.BookingTimeFinish >= endTime)) &&
+                        b.MachineType == machineType)
+                .Select(b => b.MachineNumber)
+                .ToList();
 
-              var availableMachines = allMachines
-                  .Where(m => !reservedMachines.Contains(m.Id) && m.MachineType == machineType);
+            var allMachines = _dbContext.Machines
+                .Where(m => m.MachineType == machineType)
+                .Select(m => m.MachineNumber)
+                .ToList();
 
-              return availableMachines;
-          } 
+            var availableMachines = allMachines.Except(reservedMachines).ToList();
+
+            return Json(availableMachines);
+        }
 
 
         [HttpGet]
-        public ActionResult Create()
+        public ActionResult Create(DateTime bookingTimeStart, string machineType)
         {
-            ViewBag.TimeOptions = new SelectList(new[]
-        {
-             new SelectListItem { Value = "08:00", Text = "8:00" },
-             new SelectListItem { Value = "09:00", Text = "9:00" },
-             new SelectListItem { Value = "10:00", Text = "10:00" },
-             new SelectListItem { Value = "11:00", Text = "11:00" },
-             new SelectListItem { Value = "12:00", Text = "12:00" },
-             new SelectListItem { Value = "13:00", Text = "13:00" },
-             new SelectListItem { Value = "14:00", Text = "14:00" },
-             new SelectListItem { Value = "15:00", Text = "15:00" },
-             new SelectListItem { Value = "16:00", Text = "16:00" },
-             new SelectListItem { Value = "17:00", Text = "17:00" },
-             new SelectListItem { Value = "18:00", Text = "18:00" },
-             new SelectListItem { Value = "19:00", Text = "19:00" },
-             new SelectListItem { Value = "20:00", Text = "20:00" },
-             new SelectListItem { Value = "21:00", Text = "21:00" },
-             new SelectListItem { Value = "22:00", Text = "22:00" },
-             new SelectListItem { Value = "23:00", Text = "23:00" },
-
-           }, "Value", "Text");
+          
 
             var machineTypes = _dbContext.Machines.Select(m => m.MachineType).Distinct().ToList();
+
             SelectList machineTypeList = new SelectList(machineTypes, "MachineType");
             ViewBag.MachineType = machineTypeList;
+
+          
             return View();
         }
 
         [HttpPost]
-        public ActionResult Create(Booking newBooking)
+        public ActionResult Create()
         {           
+            var newBooking = new Booking();
            
-     
             try
             {
                 if (ModelState.IsValid)
                 {
+
                     _dbContext.Bookings.Add(newBooking);
                     newBooking.BookingTimeFinish = newBooking.BookingTimeStart.AddHours(1);
                     _dbContext.SaveChanges();
 
-                    return RedirectToAction(nameof(Index));
+                    return View();
                 }
                 else
                 {
@@ -105,6 +97,11 @@ namespace LaundryReservationSystem.Controllers
             {
                 return View("Error");
             }
+        }
+
+        private ActionResult RedirectToAction(Func<ActionResult> ındex)
+        {
+            throw new NotImplementedException();
         }
 
         public ActionResult Edit(int id)
